8a4d36be5ee75d5725c22e07509c78b6afecc4c5
Seth Ringling <sringling@avvo.com>
2015-07-23 11:00:20 -0700

    Ensure meta is symbolized

--- lib/json_api_resource/queryable.rb
+++ lib/json_api_resource/queryable.rb
@@ -15,27 +15,34 @@ module JsonApiResource
         results = self.client_klass.find(id).map! do |result|
           self.new(:client => result)
         end
-        results.size == 1 ? single_result(results) : results
+        results = results.size == 1 ? single_result(results) : results
+        package_result(results)
       rescue JsonApiClient::Errors::ServerError => e
         pretty_error e
       end
 
       def create(attr = {})
         run_callbacks :create do
-          new(:client => self.client_klass.create(attr))
+          package_result(new(:client => self.client_klass.create(attr)))
         end
       end
 
       def where(opts = {})
         opts[:per_page] = opts.fetch(:per_page, self.per_page)
-        (self.client_klass.where(opts).all).map! do |result|
+        results = (self.client_klass.where(opts).all).map! do |result|
           self.new(:client => result)
         end
+        package_result(results)
       rescue JsonApiClient::Errors::ServerError => e
         pretty_error e
       end
 
       private
+
+      def package_result(result)
+        result.meta.deep_symbolize_keys!
+        result
+      end
 
       # When we return a collection, these extra attributes on top of the result array from JsonApiClient are present.
       # When we find just one thing and return the first element like ActiveRecord would,
