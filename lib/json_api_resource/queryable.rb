9b7b6b73d012c04a7ee2de5e84d17f9ee250137c
Seth Ringling <sringling@avvo.com>
2015-07-24 07:54:51 -0700

    Expect json_api_client to use a HashWithIndifferentAccess for metadata, no need to symbolize keys

--- lib/json_api_resource/queryable.rb
+++ lib/json_api_resource/queryable.rb
@@ -15,34 +15,27 @@ module JsonApiResource
         results = self.client_klass.find(id).map! do |result|
           self.new(:client => result)
         end
-        results = results.size == 1 ? single_result(results) : results
-        package_result(results)
+        results.size == 1 ? single_result(results) : results
       rescue JsonApiClient::Errors::ServerError => e
         pretty_error e
       end
 
       def create(attr = {})
         run_callbacks :create do
-          package_result(new(:client => self.client_klass.create(attr)))
+          new(:client => self.client_klass.create(attr))
         end
       end
 
       def where(opts = {})
         opts[:per_page] = opts.fetch(:per_page, self.per_page)
-        results = (self.client_klass.where(opts).all).map! do |result|
+        (self.client_klass.where(opts).all).map! do |result|
           self.new(:client => result)
         end
-        package_result(results)
       rescue JsonApiClient::Errors::ServerError => e
         pretty_error e
       end
 
       private
-
-      def package_result(result)
-        result.meta.deep_symbolize_keys!
-        result
-      end
 
       # When we return a collection, these extra attributes on top of the result array from JsonApiClient are present.
       # When we find just one thing and return the first element like ActiveRecord would,
