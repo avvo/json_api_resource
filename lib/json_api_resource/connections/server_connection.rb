29ecd27feda7bcb379e03a7b609940dddfc689f3
Greg Orlov <greg@avvo.com>
2016-04-05 11:09:36 -0700

    simplifying connections

--- lib/json_api_resource/connections/server_connection.rb
+++ lib/json_api_resource/connections/server_connection.rb
@@ -6,19 +6,21 @@ module JsonApiResource
       class_attribute :error_notifier
 
       def report_error( e )
-        unless i.is_a? ServerNotReadyError
-          error_notifier.notify( self, e ) if error_notifier.present?
-        end
+        error_notifier.notify( self, e ) if error_notifier.present?
       end
 
       def request( action, *args )
-        client_request(action, *args)
+
+        result = self.client.send action, *args
+
+        if result.is_a? JsonApiClient::Scope
+          result = result.all
+        end
+
+        result
 
       rescue JsonApiClient::Errors::NotFound => e
-        empty_set_with_errors e
-      end
 
-      def empty_set_with_errors( e )
         result = JsonApiClient::ResultSet.new
 
         result.meta = {status: 404}
@@ -27,18 +29,7 @@ module JsonApiResource
         result.errors.add("RecordNotFound", e.message)
 
         result
-      end
 
-      private
-
-      def client_request(action, *args)
-        result = self.client.send action, *args
-
-        if result.is_a? JsonApiClient::Scope
-          result = result.all
-        end
-
-        result
       end
     end
   end
