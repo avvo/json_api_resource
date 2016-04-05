7c013a260556c91f28abd7111ebb1d19aafea4e7
Greg <greg@avvo.com>
2016-04-05 10:37:58 -0700

    simplifying defaukt server connection

--- lib/json_api_resource/connections/server_connection.rb
+++ lib/json_api_resource/connections/server_connection.rb
@@ -3,17 +3,7 @@ module JsonApiResource
     class ServerConnection < Multiconnect::Connection::Base
       include Keyable
 
-      class_attribute :cache
-      class_attribute :cache_processor
       class_attribute :error_notifier
-
-      self.cache_processor = ::JsonApiResource::CacheProcessor::Base
-
-      def initialize(options)
-        super options
-        @caching            = options.fetch :caching, true
-        @timeout            = Time.now
-      end
 
       def report_error( e )
         unless i.is_a? ServerNotReadyError
@@ -22,25 +12,10 @@ module JsonApiResource
       end
 
       def request( action, *args )
-        if ready_for_request?
-          
-          result = client_request(action, *args)
-
-          cache(action, args, result) if cache_processor.cache?(action)
-
-          result
-
-        else
-          raise ServerNotReadyError
-        end
+        client_request(action, *args)
 
       rescue JsonApiClient::Errors::NotFound => e
         empty_set_with_errors e
-      rescue => e
-        @timeout = timeout
-
-        # propagate the error up to be handled by Connection::Base
-        raise e
       end
 
       def empty_set_with_errors( e )
@@ -55,30 +30,6 @@ module JsonApiResource
       end
 
       private
-
-      def timeout
-        # default circuit broken for 30 seconds. 
-        # This should probably be 1 - 2 - 5 - 15 - 30 - 1 min *
-        Time.now + 30.seconds
-      end
-
-      def ready_for_request?
-        Time.now > @timeout
-      end
-
-      def cache?
-        @caching && cache.present?
-      end
-
-      def cache(action, args, result)
-        key = cache_key(client, action, args)
-
-        if cache_processor.present?
-          result = cache_processor.process action, args, result
-        end
-
-        self.class.cache.write key, result
-      end
 
       def client_request(action, *args)
         result = self.client.send action, *args
